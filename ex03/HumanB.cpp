b46a581aa0040c653270416c558067732531d71d
misterjune <sounddevcloud145@gmail.com>
2023-10-27 03:36:01 +0700

    finish cpp01/ex03 2

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:35:18 by tponutha          #+#    #+#             */
-/*   Updated: 2023/10/27 03:28:52 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/10/27 03:35:15 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void	HumanB::attack()
 {
-	std::cout << HumanB::name << " attacks with their " << *HumanB::pwpon << std::endl;
+	if (HumanB::pwpon == NULL)
+	{
+		std::cout << HumanB::name << " has no weapon" << std::endl;
+	}
+	else
+	{
+		std::cout << HumanB::name << " attacks with their " << *HumanB::pwpon << std::endl;
+	}
 }
 
 void	HumanB::setWeapon(Weapon &etc)
