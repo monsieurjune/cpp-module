f9f36edc36972c3ceb009180a4f09733c2e13344
monsieurjune <sounddevcloud145@gmail.com>
2023-11-12 16:34:29 +0700

    finished cpp01/ex04 fix way to read file

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,11 +6,12 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 03:37:17 by tponutha          #+#    #+#             */
-/*   Updated: 2023/10/27 04:51:29 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/11/12 16:33:30 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 
 static bool	file_open(std::string name, std::ifstream &infile, std::ofstream &outfile)
@@ -18,12 +19,14 @@ static bool	file_open(std::string name, std::ifstream &infile, std::ofstream &ou
 	infile.open(name.data(), std::ios::in);
 	if (!infile.is_open())
 	{
+		std::cerr << "File : " << name << "is permission/file error" << std::endl;
 		return false;
 	}
 	name.append(".replace");
 	outfile.open(name.data(), std::ios::out | std::ios::trunc);
 	if (!outfile.is_open())
 	{
+		std::cerr << "File : " << name << "is permission/file error" << std::endl;
 		infile.close();
 		return false;
 	}
@@ -34,29 +37,52 @@ static void	write_sed(std::ofstream &outfile, std::string res, std::string s1, s
 {
 	size_t	n;
 
-	while (!res.empty())
+	while (!res.empty() && outfile.good())
 	{
 		n = res.find(s1, 0);
 		outfile << res.substr(0, n);
-		if (n == (size_t)-1){ return; }
+		if (n == (size_t)-1)
+		{
+			return;
+		}
 		outfile << s2;
 		res.erase(0, n + 1);
 	}
 }
 
-// static std::string	ft_replace(std::)
+static bool	file_read(std::ifstream &infile, std::string &res)
+{
+	std::ostringstream	tmp;
+
+	if (!infile.good())
+	{
+		return false;
+	}
+	tmp << infile.rdbuf();
+	res = tmp.str();
+	return true;
+}
 
 int	main(int ac, char **av)
 {
 	std::ofstream	outfile;
 	std::ifstream	infile;
-	std::string		res	= "";
+	std::string		res;
 	std::string		line;
 
-	if (ac != 4){ return 0; }
-	if (!file_open(av[1], infile, outfile)){ return 1; }
-	while (std::getline(infile, line)){ res.append(line); }
-	write_sed(outfile, res, av[2], av[3]);
+	if (ac != 4)
+	{
+		std::cerr << "There must be 4 arguments" << std::endl;
+		return 0;
+	}
+	if (!file_open(av[1], infile, outfile))
+	{
+		return 1;
+	}
+	if (file_read(infile, res))
+	{
+		write_sed(outfile, res, av[2], av[3]);
+	}
 	infile.close();
 	outfile.close();
 	return 0;
