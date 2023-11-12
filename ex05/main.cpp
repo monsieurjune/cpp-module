0d212b8ad04548ae172b4d593de72e27f24a1aae
monsieurjune <sounddevcloud145@gmail.com>
2023-11-12 16:52:39 +0700

    finished cpp01/ex05 make harl more 'karen' friendly

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 03:19:00 by tponutha          #+#    #+#             */
-/*   Updated: 2023/11/09 04:08:41 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/11/12 16:50:59 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,35 @@ static bool	prompt(std::string prompt, std::string &line)
 	return !std::getline(std::cin, line);
 }
 
+static void tutorial()
+{
+	std::cout << "Welcome to Harl 2.0 here 4 level of command" << std::endl;
+	std::cout << "- level1 : debug" << std::endl;
+	std::cout << "- level2 : info" << std::endl;
+	std::cout << "- level3 : warning" << std::endl;
+	std::cout << "- level4 : error" << std::endl;
+}
+
 int	main()
 {
 	Harl			run;
+	size_t			first;
+	size_t			last;
 	std::string		line;
 
+	tutorial();
 	while (!prompt("Harl> ", line))
 	{
+		first = line.find_first_not_of(' ');
+		last = line.find_last_not_of(' ');
+		if (first <= line.length())
+		{
+			line = line.substr(first, last - first + 1);
+		}
+		else
+		{
+			line = "";
+		}
 		run.complain(line);
 	}
 	return (0);
