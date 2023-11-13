f607583c6429300818c7e24b731919c3d3fcd697
misterjune <sounddevcloud145@gmail.com>
2023-11-13 18:23:54 +0700

    finish cpp03/ex00 2

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 17:13:13 by tponutha          #+#    #+#             */
-/*   Updated: 2023/11/13 18:15:24 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:23:22 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,18 @@ ClapTrap::ClapTrap(std::string name)
 	ClapTrap::HitPoint = 10;
 	ClapTrap::EnergyPoint = 10;
 	ClapTrap::AttackDamage = 0;
+	std::cout << "ClapTrap " << name << " is born with " 
+			<< ClapTrap::HitPoint << " HitPoint, "
+			<< ClapTrap::EnergyPoint << " EnergyPoint, "
+			<< ClapTrap::AttackDamage << " AttackDamage" << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
 	if (ClapTrap::HitPoint <= 0 || ClapTrap::EnergyPoint <= 0)
 		return;
-	std::cout << "ClapTrap " << ClapTrap::name << " attacks " << target << ", ";
-	std::cout << "causing " << ClapTrap::AttackDamage << " points of damage!" << std::endl;
+	std::cout << "ClapTrap " << ClapTrap::name << " attacks " << target << ", "
+			<< "causing " << ClapTrap::AttackDamage << " points of damage!" << std::endl;
 	ClapTrap::EnergyPoint--;
 }
 
@@ -34,9 +38,9 @@ void	ClapTrap::takeDamage(unsigned int amount)
 	if (ClapTrap::HitPoint <= 0 || ClapTrap::EnergyPoint <= 0)
 		return;
 	ClapTrap::HitPoint -= amount;
-	std::cout << "ClapTrap " << ClapTrap::name << " take " << amount << " damage, ";
-	std::cout << "causing ClapTrap " << ClapTrap::name << " has " << ClapTrap::HitPoint;
-	std::cout << " Hitpoint left " << std::endl;
+	std::cout << "ClapTrap " << ClapTrap::name << " take " << amount << " damage, "
+			<< "causing ClapTrap " << ClapTrap::name << " has " << ClapTrap::HitPoint
+			<< " Hitpoint left " << std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
@@ -45,7 +49,7 @@ void	ClapTrap::beRepaired(unsigned int amount)
 		return;
 	ClapTrap::HitPoint += amount;
 	ClapTrap::EnergyPoint--;
-	std::cout << "ClapTrap " << ClapTrap::name << " repair itself with " << amount << " point, ";
-	std::cout << "causing ClapTrap " << ClapTrap::name << " has " << ClapTrap::HitPoint << " Hitpoint, ";
-	std::cout << "and has " << ClapTrap::EnergyPoint << " Energy left" << std::endl;
+	std::cout << "ClapTrap " << ClapTrap::name << " repair itself with " << amount << " point, "
+			<< "causing ClapTrap " << ClapTrap::name << " has " << ClapTrap::HitPoint << " Hitpoint, "
+			<< "and has " << ClapTrap::EnergyPoint << " Energy left" << std::endl;
 }
