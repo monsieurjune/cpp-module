9538f3351dd8394c75c2a834eeee7e76de8f1212
misterjune <sounddevcloud145@gmail.com>
2023-11-13 20:20:36 +0700

    finish cpp03/ex01 2

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 18:17:39 by tponutha          #+#    #+#             */
-/*   Updated: 2023/11/13 20:06:03 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/11/13 20:18:24 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ ScavTrap::~ScavTrap()
 
 void	ScavTrap::guardGate()
 {
+	if (ScavTrap::HitPoint <= 0 || ScavTrap::EnergyPoint <= 0)
+		return;
 	std::cout << "ScavTrap " <<  ScavTrap::name << " is now in Gate keeper mode." << std::endl;
 }
 
