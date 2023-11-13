2803e5c3f04939ff6d89bede7b5a3a2fba4d44ce
misterjune <sounddevcloud145@gmail.com>
2023-11-13 16:47:02 +0700

    finish cpp02/ex02 2

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 00:38:02 by tponutha          #+#    #+#             */
-/*   Updated: 2023/11/13 16:38:12 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:46:32 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,30 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( -5.05f ) / Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
+	Fixed const c( Fixed( -5.05f ) + Fixed( 2 ));
+
+	std::cout << "Before : " <<  a << std::endl;
+	std::cout << "Pre-inc : " << ++a << std::endl;
+	std::cout << "Before : " << a << std::endl;
+	std::cout << "Post-inc : " << a++ << std::endl;
+	std::cout << "A : " << a << std::endl;
+	std::cout << "B : " << b << std::endl;
+	std::cout << "C : " << c << std::endl;
+	std::cout << "Max : " << Fixed::max(a, b) << std::endl;
+
+	bool	res;
+
+	res = a == b;
+	std::cout << "A == B : " << res<< std::endl;
+	res = a != b;
+	std::cout << "A != B : " << res<< std::endl;
+	res = a > b;
+	std::cout << "A > B : " << res<< std::endl;
+	res = a >= b;
+	std::cout << "A >= B : " << res<< std::endl;
+	res = a < b;
+	std::cout << "A < B : " << res<< std::endl;
+	res = a <= b;
+	std::cout << "A <= B : " << res<< std::endl;
 	return 0;
 }
