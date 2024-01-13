8d4b0d2d2f7ca812605d2c56b570989c7c048dea
misterjune <sounddevcloud145@gmail.com>
2024-01-13 07:24:22 +0700

    finish cpp04/ex00 2

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 23:39:42 by tponutha          #+#    #+#             */
-/*   Updated: 2024/01/13 05:38:13 by tponutha         ###   ########.fr       */
+/*   Updated: 2024/01/13 07:18:48 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class Animal
 		Animal(std::string type);
 		virtual	~Animal();
 		std::string		getType()	const;
-		virtual void	makeSound()	const;
+		void			makeSound()	const;
 
 	protected:
 		std::string	type;
