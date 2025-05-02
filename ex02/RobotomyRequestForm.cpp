7fa597a0219287e5bf712d1532f4767a17ed7e56
monsieurjune <june.monsieurjune@gmail.com>
2025-05-02 13:13:05 +0700

    (feat): Add RobotomyRequestForm in ex02/

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -6,8 +6,99 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 14:07:57 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/03 14:15:10 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/04/04 14:29:45 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Project Header
 #include "RobotomyRequestForm.hpp"
+
+// C Header
+#include <ctime>
+#include <cstdlib>
+
+// CPP Header
+#include <iostream>
+
+RobotomyRequestForm::RobotomyRequestForm() 
+                        : AForm(
+                            __ROBOTOMY_REQUEST_FORM_DEFAULT_NAME__,
+                            __ROBOTOMY_REQUEST_FORM_SIGN_GRADE__,
+                            __ROBOTOMY_REQUEST_FORM_EXECUTE_GRADE__
+                        )
+{
+    std::cout << "Robotomy Request Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+                << ") is created from default constructor program" << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string const& target)
+                        : AForm(
+                            __ROBOTOMY_REQUEST_FORM_DEFAULT_NAME__,
+                            __ROBOTOMY_REQUEST_FORM_SIGN_GRADE__,
+                            __ROBOTOMY_REQUEST_FORM_EXECUTE_GRADE__
+                        )
+{
+    std::cout << "Robotomy Request Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is created from constructor program" << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const& obj) : AForm(obj)
+{
+    std::cout << "Robotomy Request Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is copied" << std::endl;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+    std::cout << "Robotomy Request Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is discarded" << std::endl;
+}
+
+RobotomyRequestForm&    RobotomyRequestForm::operator=(RobotomyRequestForm const& rhs)
+{
+    std::cout << "Copy Signature of Presidential Pardon Form <" 
+                << rhs.getName() << "> (" << rhs.getSignGradeThreshold() 
+                << "," << rhs.getExecuteGradeThreshold() 
+                << ") (" << (rhs.getIsSigned() ? "signed" : "unsigned") 
+                << ") to Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") << ")" << std::endl;
+
+    AForm::operator=(rhs);
+
+    return *this;
+}
+
+void    RobotomyRequestForm::execute(Bureaucrat const& executor) const
+{
+    // is sign yet
+    if (!this->getIsSigned())
+    {
+        throw AForm::UnsignedFormException();
+    }
+
+    // lower in reverse way
+    if (executor.getGrade() > this->getExecuteGradeThreshold())
+    {
+        throw AForm::GradeTooLowException("execute");
+    }
+
+    std::srand(std::time(NULL));
+    if (std::rand() % 2)
+    {
+        throw AForm::ExecutionFailedException("the robotomy failed");
+    }
+
+    std::cout << "b.r..r.zzz.zz...." << std::endl;
+    std::cout << "B.r.R..zzZ..ZZ.." << std::endl;
+    std::cout << "BBRRRRZZZZ......" << std::endl;
+    std::cout << this->getName() << " has been robotomized successfully 50% of the time" << std::endl;
+}
--- ex02/RobotomyRequestForm.hpp
+++ ex02/RobotomyRequestForm.hpp
@@ -6,13 +6,25 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 11:41:23 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/03 13:58:05 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/04/03 16:44:38 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __ROBOTOMY_REQUEST_FORM_HPP__
 #define __ROBOTOMY_REQUEST_FORM_HPP__
 #include "AForm.hpp"
+
+#ifndef __ROBOTOMY_REQUEST_FORM_DEFAULT_NAME__
+#define __ROBOTOMY_REQUEST_FORM_DEFAULT_NAME__ "Robot Request"
+#endif
+
+#ifndef __ROBOTOMY_REQUEST_FORM_SIGN_GRADE__
+#define __ROBOTOMY_REQUEST_FORM_SIGN_GRADE__ 72
+#endif
+
+#ifndef __ROBOTOMY_REQUEST_FORM_EXECUTE_GRADE__
+#define __ROBOTOMY_REQUEST_FORM_EXECUTE_GRADE__ 45
+#endif
 
 class RobotomyRequestForm : public AForm
 {
