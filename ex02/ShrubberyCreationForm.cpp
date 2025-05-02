ed2c0bb22bbfbe2fc727151c96cfde6dfa18c19e
monsieurjune <june.monsieurjune@gmail.com>
2025-05-02 17:06:06 +0700

    (feat): Add ShruberryCreationForm in ex02/

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,8 +6,125 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 14:08:18 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/03 14:15:19 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/05/02 17:02:16 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Project Header
 #include "ShrubberyCreationForm.hpp"
+
+// CPP Header
+#include <iostream>
+#include <fstream>
+
+ShrubberyCreationForm::ShrubberyCreationForm()
+                            : AForm(
+                                __SHRUBBERY_CREATION_FORM_DEFAULT_NAME__,
+                                __SHRUBBERY_CREATION_FORM_SIGN_GRADE__,
+                                __SHRUBBERY_CREATION_FORM_EXECUTE_GRADE__
+                            )
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+                << ") is created from default constructor program" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string const& target)
+                            : AForm(
+                                target,
+                                __SHRUBBERY_CREATION_FORM_SIGN_GRADE__,
+                                __SHRUBBERY_CREATION_FORM_EXECUTE_GRADE__
+                            )
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+                << ") is created from default constructor program" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const& obj) : AForm(obj)
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is copied" << std::endl;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is discarded" << std::endl;
+}
+
+ShrubberyCreationForm   ShrubberyCreationForm::operator=(ShrubberyCreationForm const& rhs)
+{
+    std::cout << "Copy Signature of Presidential Pardon Form <" 
+                << rhs.getName() << "> (" << rhs.getSignGradeThreshold() 
+                << "," << rhs.getExecuteGradeThreshold() 
+                << ") (" << (rhs.getIsSigned() ? "signed" : "unsigned") 
+                << ") to Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") << ")" << std::endl;
+
+    AForm::operator=(rhs);
+
+    return *this;
+}
+
+void    ShrubberyCreationForm::execute(Bureaucrat const& executor) const
+{
+    // is sign yet
+    if (!this->getIsSigned())
+    {
+        throw AForm::UnsignedFormException();
+    }
+
+    // lower in reverse way
+    if (executor.getGrade() > this->getExecuteGradeThreshold())
+    {
+        throw AForm::GradeTooLowException("execute");
+    }
+
+    // execute
+    std::ofstream   shrubbery(
+                        this->getName() + "_shrubbery", 
+                        std::ios::out | std::ios::trunc
+                    );
+
+    if (!shrubbery)
+    {
+        throw AForm::ExecutionFailedException(
+                    "Can\'t open " + this->getName() + "_shrubbery"
+                );
+    }
+
+    // create ascii tree
+    shrubbery << "          &&& &&  & &&" << std::endl;
+    shrubbery << "      && &\\/&\\|& ()|/ @, &&" << std::endl;
+    shrubbery << "      &\\/(/&/&||/& /_/)_&/_&" << std::endl;
+    shrubbery << "   &() &\\/&|()|/&\\/ '%\" & ()" << std::endl;
+    shrubbery << "  &_\\_&&_\\ |& |&&/&__%_/_& &&" << std::endl;
+    shrubbery << "&&   && & &| &| /& & % ()& /&&" << std::endl;
+    shrubbery << " ()&_---()&\\&\\|&&-&&--%---()~" << std::endl;
+    shrubbery << "     &&     \\|||" << std::endl;
+    shrubbery << "             |||" << std::endl;
+    shrubbery << "             |||" << std::endl;
+    shrubbery << "             |||" << std::endl;
+    shrubbery << "       , -=-~  .-^- _" << std::endl;
+    shrubbery << "ejm97         `" << std::endl;
+
+    // check fail & bad bits
+    if (shrubbery.fail())
+    {
+        shrubbery.close();
+        throw AForm::ExecutionFailedException(
+                "IO Error occured when writing " + this->getName() + "_shrubbery"
+            );
+    }
+
+    // close file
+    shrubbery.close();
+}
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -6,13 +6,25 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 11:41:18 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/03 14:06:41 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/04/03 16:53:12 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SHRUBBERY_CREATION_FORM_HPP__
 #define __SHRUBBERY_CREATION_FORM_HPP__
 #include "AForm.hpp"
+
+#ifndef __SHRUBBERY_CREATION_FORM_DEFAULT_NAME__
+#define __SHRUBBERY_CREATION_FORM_DEFAULT_NAME__ "Shrubbery"
+#endif
+
+#ifndef __SHRUBBERY_CREATION_FORM_SIGN_GRADE__
+#define __SHRUBBERY_CREATION_FORM_SIGN_GRADE__ 145
+#endif
+
+#ifndef __SHRUBBERY_CREATION_FORM_EXECUTE_GRADE__
+#define __SHRUBBERY_CREATION_FORM_EXECUTE_GRADE__ 137
+#endif
 
 class ShrubberyCreationForm : public AForm
 {
