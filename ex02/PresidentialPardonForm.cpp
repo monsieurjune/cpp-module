89916eee4ad8eec426c56fec0fded846d99c7b5b
monsieurjune <june.monsieurjune@gmail.com>
2025-05-02 13:12:28 +0700

    (feat): Add PresidentialPardonForm in ex02/

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -6,8 +6,86 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 14:07:54 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/03 14:14:58 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/04/04 14:37:07 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Project Header
 #include "PresidentialPardonForm.hpp"
+
+// CPP Header
+#include <iostream>
+
+PresidentialPardonForm::PresidentialPardonForm()
+                            : AForm(
+                                    __PRESIDENTIAL_PARDON_FORM_DEFAULT_NAME__,
+                                    __PRESIDENTIAL_PARDON_FORM_SIGN_GRADE__,
+                                    __PRESIDENTIAL_PARDON_FORM_EXECUTE_GRADE__
+                                )
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+                << ") is created from default constructor program" << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string const& target)
+                            : AForm(
+                                    target,
+                                    __PRESIDENTIAL_PARDON_FORM_SIGN_GRADE__,
+                                    __PRESIDENTIAL_PARDON_FORM_EXECUTE_GRADE__
+                                )
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+                << ") is created from default constructor program" << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const& obj) : AForm(obj)
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is copied" << std::endl;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+    std::cout << "Presidential Pardon Form <" << this->getName() << "> (" 
+            << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+            << ") (" << (this->getIsSigned() ? "signed" : "unsigned") 
+            << ") is discarded" << std::endl;
+}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(PresidentialPardonForm const& rhs)
+{
+    std::cout << "Copy Signature of Presidential Pardon Form <" 
+                << rhs.getName() << "> (" << rhs.getSignGradeThreshold() 
+                << "," << rhs.getExecuteGradeThreshold() 
+                << ") (" << (rhs.getIsSigned() ? "signed" : "unsigned") 
+                << ") to Form <" << this->getName() << "> (" 
+                << this->getSignGradeThreshold() << "," << this->getExecuteGradeThreshold() 
+                << ") (" << (this->getIsSigned() ? "signed" : "unsigned") << ")" << std::endl;
+
+    AForm::operator=(rhs);
+
+    return *this;
+}
+
+void    PresidentialPardonForm::execute(Bureaucrat const& executor) const
+{
+    // is sign yet
+    if (!this->getIsSigned())
+    {
+        throw AForm::UnsignedFormException();
+    }
+
+    // lower in reverse way
+    if (executor.getGrade() > this->getExecuteGradeThreshold())
+    {
+        throw AForm::GradeTooLowException("execute");
+    }
+
+    std::cout << this->getName() << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
