09a0d97b478e831e509d3a82b0babe84c58d083f
monsieurjune <june.monsieurjune@gmail.com>
2025-05-28 15:49:51 +0700

    (feat): Make classes in ex00/ & ex01/ insanitiable & remove -pdantic

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 11:45:53 by tponutha          #+#    #+#             */
-/*   Updated: 2025/05/19 11:47:12 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:44:18 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 
 class ScalarConverter
 {
+    private:
+        ScalarConverter();
+
     public:
         static void convert(std::string const& str);
 };
--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:14:30 by tponutha          #+#    #+#             */
-/*   Updated: 2025/05/20 12:41:18 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:46:23 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 
 class Serializer
 {
+    private:
+        Serializer();
+
     public:
         static uintptr_t    serialize(Data* ptr);
         static Data*        deserialize(uintptr_t raw);
