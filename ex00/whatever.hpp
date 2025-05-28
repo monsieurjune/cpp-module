5a94412eb500aa3b9ca11edb6b5e7763f1f1e7bf
monsieurjune <june.monsieurjune@gmail.com>
2025-05-28 16:01:53 +0700

    (feat): finish ex00/ & add ignore files in .gitignore

---
+++ ex00/whatever.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 15:54:20 by tponutha          #+#    #+#             */
+/*   Updated: 2025/05/28 16:01:24 by tponutha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __WHATEVER_HPP__
+#define __WHATEVER_HPP__
+
+template <typename T> void swap(T& a, T& b)
+{
+    T   temp = a;
+
+    a = b;
+    b = temp;
+}
+
+template <typename T> T min(T a, T b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    return b;
+}
+
+template <typename T> T max(T a, T b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+#endif
