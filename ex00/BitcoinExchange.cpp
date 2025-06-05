e93ab531ba7778d9984264fced08ef0979916c00
monsieurjune <june.monsieurjune@gmail.com>
2025-06-05 07:38:24 +0700

    (feat): Create BitcoinExchange class in ex00/
    Create BitcointExchange class with following thing
    - Implement Orthodox canonical form stuff
    - Implement getter
    - Define parser's methods

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,8 +6,46 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 02:28:48 by tponutha          #+#    #+#             */
-/*   Updated: 2025/06/04 02:30:29 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/06/05 07:38:13 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() : _isObjValid(true) {}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const& rhs) : _mapPriceByDate(rhs.getMapPriceByDate()),\
+                                                                _isObjValid(rhs.isValid())
+{
+}
+
+BitcoinExchange::BitcoinExchange(std::ifstream const& dbfile)
+{
+    readDB(dbfile);
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange&    BitcoinExchange::operator=(BitcoinExchange const& rhs)
+{
+    if (&rhs == this)
+    {
+        return *this;
+    }
+
+    _mapPriceByDate = rhs.getMapPriceByDate();
+    _isObjValid = rhs.isValid();
+
+    return *this;
+}
+
+bool    BitcoinExchange::isValid() const
+{
+    return _isObjValid;
+}
+
+std::map<std::string, double> const&    BitcoinExchange::getMapPriceByDate() const
+{
+    return _mapPriceByDate;
+}
+
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,12 +6,44 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 02:28:45 by tponutha          #+#    #+#             */
-/*   Updated: 2025/06/04 02:30:21 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/06/05 07:37:46 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __BITCOIN_EXCHANGE_HPP__
 #define __BITCOIN_EXCHANGE_HPP__
+#include <string>
+#include <fstream>
+#include <set>
+#include <map>
 
+class BitcoinExchange
+{
+    private:
+        bool                            _isObjValid;
+        std::map<std::string, double>   _mapPriceByDate;
+
+        void    checkDateFormat(std::string const& date);
+
+        void    checkCSVHeader(std::string const& line);
+        void    checkCSVLine(std::string const& line);
+
+        void    checkInputHeader(std::string const& line);
+        void    checkInputLine(std::string const& line);
+
+    public:
+        BitcoinExchange();
+        BitcoinExchange(std::ifstream const& dbfile);
+        BitcoinExchange(BitcoinExchange const& rhs);
+        ~BitcoinExchange();
+        BitcoinExchange&    operator=(BitcoinExchange const& rhs);
+
+        bool                                    isValid() const;
+        std::map<std::string, double> const&    getMapPriceByDate() const;
+
+        void    readDB(std::ifstream const& dbfile);
+        void    analyze(std::ifstream const& infile);
+
+};
 
 #endif
